b1252fca80ad77e3bd2cc4c402fb2de0801829e4
Marko Krstic <markokrstic001@gmail.com>
2024-03-11 16:28:28 +0100

    blog folder without front

--- src/Explorer.API/Controllers/BlogController.cs
+++ src/Explorer.API/Controllers/BlogController.cs
@@ -20,5 +20,128 @@ namespace Explorer.API.Controllers
 
         
 
+
+        private readonly IBlogService _blogService;
+        private readonly IClubMemberManagementService _clubMemberManagmentService;
+        private readonly IClubService _clubService;
+
+        public BlogController(IBlogService authenticationService, IClubMemberManagementService clubMemberManagmentService, IClubService clubService)
+        {
+            _blogService = authenticationService;
+            _clubMemberManagmentService = clubMemberManagmentService;
+            _clubService = clubService;
+        }
+
+
+        [Authorize(Policy = "userPolicy")]
+        [HttpPost("create")]
+        public ActionResult<BlogResponseDto> Create([FromBody] BlogCreateDto blog)
+        {
+            blog.Date = DateTime.UtcNow;
+            blog.AuthorId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            var result = _blogService.Create(blog);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "userPolicy")]
+        [HttpPut("update")]
+        public ActionResult<BlogResponseDto> Update([FromBody] BlogUpdateDto blog)
+        {
+            blog.AuthorId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            var result = _blogService.Update(blog);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "userPolicy")]
+        [HttpDelete("delete/{id:long}")]
+        public ActionResult<BlogResponseDto> Delete(int id)
+        {
+            var result = _blogService.Delete(id);
+            return CreateResponse(result);
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<BlogResponseDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _blogService.GetAll(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("{id:long}")]
+        public ActionResult<BlogResponseDto> Get(long id)
+        {
+            var result = _blogService.GetById(id);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<BlogResponseDto> Update([FromBody] BlogUpdateDto blog, int id)
+        {
+            blog.Id = id;
+            var result = _blogService.UpdateBlog(blog);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "userPolicy")]
+        [HttpGet("upvote/{id:long}")]
+        public ActionResult Upvote(long id)
+        {
+            if (_blogService.IsBlogClosed(id)) return CreateResponse(Result.Fail(FailureCode.InvalidArgument));
+
+            var userId = long.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            var result = _blogService.SetVote(id, userId, VoteType.UPVOTE);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "userPolicy")]
+        [HttpGet("downvote/{id:long}")]
+        public ActionResult Downvote(long id)
+        {
+            if (_blogService.IsBlogClosed(id)) return CreateResponse(Result.Fail(FailureCode.InvalidArgument));
+
+            var userId = long.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            var result = _blogService.SetVote(id, userId, VoteType.DOWNVOTE);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "touristPolicy")]
+        [HttpPost("createClubBlog")]
+        public ActionResult<BlogResponseDto> CreateClubBlog([FromBody] BlogCreateDto blog)
+        {
+            blog.AuthorId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            if(blog.ClubId == null)
+            {
+                return BadRequest();
+            }
+            bool touristInClub = false;
+            foreach(int touristId in _clubMemberManagmentService.GetMembers((long)blog.ClubId).Value.Results.Select(member => member.UserId))
+            {
+                if(touristId == blog.AuthorId)
+                {
+                    touristInClub = true;
+                    break;
+                }
+            }
+            if (!touristInClub)
+            {
+                if(_clubService.GetById((int)blog.ClubId).Value.OwnerId != blog.AuthorId)
+                {
+                    return BadRequest();
+                }
+            }
+            blog.Date = DateTime.UtcNow;
+            var result = _blogService.Create(blog);
+            return CreateResponse(result);
+        }
+
+        [Authorize(Policy = "touristPolicy")]
+        [HttpGet("getClubBlogs")]
+        public ActionResult<BlogResponseDto> GetClubBlogs([FromQuery] int page, [FromQuery] int pageSize, long clubId)
+        {
+            var result = _blogService.GetAllFromBlogs(page, pageSize, clubId);
+            return CreateResponse(result);
+        }
+    }
+
 }
 
