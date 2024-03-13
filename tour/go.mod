87e8a97cec55cbf4d95eadc3b1db73f525119132
Marko Čekerinac <marko.cekerinac@gmail.com>
2024-03-13 21:53:06 +0100

    fixed some error conversion to string array and time

--- tour/go.mod
+++ tour/go.mod
@@ -3,17 +3,21 @@ module tour
 go 1.22.1
 
 require (
-	github.com/gorilla/mux v1.8.1	
+	github.com/gorilla/mux v1.8.1
+	gorm.io/datatypes v1.2.0
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.7
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	gorm.io/driver/mysql v1.4.7 // indirect
 )
--- tour/model/Tour.go
+++ tour/model/Tour.go
@@ -1,7 +1,11 @@
 package model
 
 import (
-	"time"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+
+	"github.com/lib/pq"
 )
 
 type TourStatus int
@@ -23,38 +27,59 @@ const (
 
 type Tour struct {
 	Id          int            `json:"Id" gorm:"column:Id;primaryKey"`
-	AuthorID    int            `json:"AuthorId"`
-	Name        string         `json:"Name"`
-	Description string         `json:"Description"`
-	Difficulty  int            `json:"Difficulty"`
-	Tags        []string       `json:"Tags" gorm:"type:text[]"`
-	Status      TourStatus     `json:"Status"`
-	Price       float64        `json:"Price"`
-	Distance    float64        `json:"Distance"`
-	PublishDate time.Time      `json:"PublishDate" gorm:"type:time"`
-	ArchiveDate time.Time      `json:"ArchiveDate" gorm:"type:time"`
-	KeyPoints   []KeyPoint     `json:"KeyPoints" gorm:"type:jsonb"`
-	Durations   []TourDuration `json:"Durations" gorm:"type:jsonb"`
-	Reviews     []Review       `json:"Reviews" gorm:"type:jsonb"`
-	Category    TourCategory   `json:"Category"`
+	AuthorID    int            `json:"AuthorId" gorm:"column:AuthorID"`
+	Name        string         `json:"Name" gorm:"column:Name"`
+	Description string         `json:"Description" gorm:"column:Description"`
+	Difficulty  int            `json:"Difficulty" gorm:"column:Difficulty"`
+	Tags        pq.StringArray `json:"Tags" gorm:"column:Tags;type:text[]"`
+	Status      TourStatus     `json:"Status" gorm:"column:Status"`
+	Price       float64        `json:"Price" gorm:"column:Price"`
+	Distance    float64        `json:"Distance" gorm:"column:Distance"`
+	PublishDate pq.NullTime    `json:"PublishDate" gorm:"column:PublishDate;type:time"`
+	ArchiveDate pq.NullTime    `json:"ArchiveDate" gorm:"column:ArchiveDate;type:time"`
+	KeyPoints   []KeyPoint     `json:"KeyPoints" gorm:"column:KeyPoints;type:jsonb"` //pogledati za kasnije kako povezati dve tabele
+	Durations   []TourDuration `json:"Durations" gorm:"column:Durations;type:jsonb"`
+	Reviews     []Review       `json:"Reviews" gorm:"column:Reviews;type:jsonb"`
+	Category    TourCategory   `json:"Category" gorm:"column:Category"`
 }
 
 func (Tour) TableName() string {
 	return `tours."Tours"`
 }
 
+// // StringArray represents a PostgreSQL array of text
+// type StringArray []string
+
+// // Value converts StringArray to a driver Value
 // func (a StringArray) Value() (driver.Value, error) {
-// 	return json.Marshal(a)
+// 	return pq.Array(a).Value()
 // }
 
-// func (a *StringArray) Scan(value interface{}) error {
-// 	if value == nil {
+// // Scan converts a database column value to a StringArray
+// func (a *StringArray) Scan(src interface{}) error {
+// 	if src == nil {
 // 		*a = nil
 // 		return nil
 // 	}
-// 	b, ok := value.([]byte)
-// 	if !ok {
-// 		return errors.New("type assertion to []byte failed")
+// 	var array pq.StringArray
+// 	if err := array.Scan(src); err != nil {
+// 		return err
 // 	}
-// 	return json.Unmarshal(b, a)
+// 	*a = []string(array)
+// 	return nil
 // }
+
+func (td *TourDuration) Scan(value interface{}) error {
+	if value == nil {
+		return nil
+	}
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal JSONB value")
+	}
+	return json.Unmarshal(bytes, td)
+}
+
+func (td TourDuration) Value() (driver.Value, error) {
+	return json.Marshal(td)
+}
