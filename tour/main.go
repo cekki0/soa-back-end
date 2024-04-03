a780ac71b7ef4240d848d2368d70a2c60dbab662
gagi155 <maslac.ra151.2020@gmail.com>
2024-04-03 23:27:05 +0200

    add cors

--- tour/main.go
+++ tour/main.go
@@ -8,6 +8,7 @@ import (
 	"tour/repo"
 	"tour/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -62,6 +63,8 @@ func startServer(tourHandler *handler.TourHandler, reviewHandler *handler.Review
 
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
 	println("Server is running")
+	router.Use(cors.Default())
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
