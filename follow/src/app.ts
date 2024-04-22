137d0b9e8ced6031603da938b9299086e9627db4
gagi155 <maslac.ra151.2020@gmail.com>
2024-04-22 15:44:05 +0200

    add script for creating relations in neo4j

--- follow/src/app.ts
+++ follow/src/app.ts
@@ -23,40 +23,40 @@ import FollowService from "./service/follow.service";
 app.use("/example", router);
 
 app.listen(port, async () => {
-  logger.info(`Example service running on port ${port}`);
+    logger.info(`Example service running on port ${port}`);
 });
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("ez");
-  await service.createUser({ id: 1, username: "Sima" });
-  await service.createUser({ id: 2, username: "Ce" });
-  await service.createUser({ id: 3, username: "Drazen" });
-  await service.createUser({ id: 4, username: "Krle" });
+    res.send("ez");
+    // await service.createUser({ id: 1 });
+    // await service.createUser({ id: 2 });
+    // await service.createUser({ id: 3 });
+    // await service.createUser({ id: 4 });
 
-  await service.followUser(1, 2);
-  await service.followUser(1, 3);
-  await service.followUser(1, 4);
-  await service.followUser(2, 1);
-  await service.followUser(3, 1);
-  await service.followUser(4, 1);
-  await service.followUser(2, 3);
+    // await service.followUser(1, 2);
+    // await service.followUser(1, 3);
+    // await service.followUser(1, 4);
+    // await service.followUser(2, 1);
+    // await service.followUser(3, 1);
+    // await service.followUser(4, 1);
+    // await service.followUser(2, 3);
 
-  // log svih siminih followera
-  let simaFollowers = await service.getUserFollowers(1);
-  console.log("Sima followers:", simaFollowers);
+    // // log svih siminih followera
+    // let simaFollowers = await service.getUserFollowers(1);
+    // console.log("Sima followers:", simaFollowers);
 
-  // log svih siminih followera nakon sto ga ce unfollow
-  await service.unfollowUser(2, 1);
-  simaFollowers = await service.getUserFollowers(1);
-  console.log("Sima followers after unfollow:", simaFollowers);
+    // // log svih siminih followera nakon sto ga ce unfollow
+    // await service.unfollowUser(2, 1);
+    // simaFollowers = await service.getUserFollowers(1);
+    // console.log("Sima followers after unfollow:", simaFollowers);
 
-  let followedBySima = await service.getUsersFollowedByUser(1);
-  console.log("Followed by Sima:", followedBySima);
+    // let followedBySima = await service.getUsersFollowedByUser(1);
+    // console.log("Followed by Sima:", followedBySima);
 
-  await service.unfollowUser(1, 2);
-  await service.unfollowUser(1, 3);
-  followedBySima = await service.getUsersFollowedByUser(1);
-  console.log("Followed by Sima after unfollowing:", followedBySima);
+    // await service.unfollowUser(1, 2);
+    // await service.unfollowUser(1, 3);
+    // followedBySima = await service.getUsersFollowedByUser(1);
+    // console.log("Followed by Sima after unfollowing:", followedBySima);
 });
 
 const service = new FollowService();
--- follow/src/service/follow.service.ts
+++ follow/src/service/follow.service.ts
@@ -3,117 +3,115 @@ import driver from "../utils/db-connection";
 import logger from "../utils/logger";
 
 export default class FollowService {
-  async createUser(user: User): Promise<void> {
-    const session = driver.session();
+    async createUser(user: User): Promise<void> {
+        const session = driver.session();
 
-    try {
-      const result = await session.run(
-        `
-          CREATE (u:User {id: $id, username: $name})
+        try {
+            const result = await session.run(
+                `
+          CREATE (u:User {id: $id})
         `,
-        { id: user.id, name: user.username }
-      );
-    } catch (error) {
-      logger.error("Error creating user:", error);
-      throw error;
-    } finally {
-      await session.close();
+                { id: user.id }
+            );
+        } catch (error) {
+            logger.error("Error creating user:", error);
+            throw error;
+        } finally {
+            await session.close();
+        }
     }
-  }
 
-  async followUser(userId: number, followUserId: number): Promise<void> {
-    const session = driver.session();
-    try {
-      await session.run(
-        `
+    async followUser(userId: number, followUserId: number): Promise<void> {
+        const session = driver.session();
+        try {
+            await session.run(
+                `
           MATCH (u:User {id: $userId}), (fu:User {id: $followUserId})
           MERGE (u)-[:FOLLOWS]->(fu)
         `,
-        { userId, followUserId }
-      );
-    } catch (error) {
-      logger.error("Error following user:", error);
-      throw error;
-    } finally {
-      await session.close();
+                { userId, followUserId }
+            );
+        } catch (error) {
+            logger.error("Error following user:", error);
+            throw error;
+        } finally {
+            await session.close();
+        }
     }
-  }
 
-  async unfollowUser(userId: number, followUserId: number): Promise<void> {
-    const session = driver.session();
-    try {
-      await session.run(
-        `
+    async unfollowUser(userId: number, followUserId: number): Promise<void> {
+        const session = driver.session();
+        try {
+            await session.run(
+                `
           MATCH (u:User {id: $userId})-[r:FOLLOWS]->(fu:User {id: $followUserId})
           DELETE r
         `,
-        { userId, followUserId }
-      );
-    } catch (error) {
-      logger.error("Error unfollowing user:", error);
-      throw error;
-    } finally {
-      await session.close();
+                { userId, followUserId }
+            );
+        } catch (error) {
+            logger.error("Error unfollowing user:", error);
+            throw error;
+        } finally {
+            await session.close();
+        }
     }
-  }
 
-  async getUserFollowers(userId: number): Promise<User[]> {
-    const session = driver.session();
-    let followers: User[] = [];
-    try {
-      const result = await session.run(
-        `
+    async getUserFollowers(userId: number): Promise<User[]> {
+        const session = driver.session();
+        let followers: User[] = [];
+        try {
+            const result = await session.run(
+                `
           MATCH (follower:User)-[:FOLLOWS]->(u:User)
           WHERE u.id = $userId
           RETURN follower
         `,
-        { userId }
-      );
+                { userId }
+            );
 
-      followers = result.records.map((record) => {
-        const follower = record.get("follower");
-        return {
-          id: follower.properties.id,
-          username: follower.properties.username,
-        };
-      });
-    } catch (error) {
-      logger.error("Error getting user followers:", error);
-      throw error;
-    } finally {
-      await session.close();
+            followers = result.records.map((record) => {
+                const follower = record.get("follower");
+                return {
+                    id: follower.properties.id,
+                };
+            });
+        } catch (error) {
+            logger.error("Error getting user followers:", error);
+            throw error;
+        } finally {
+            await session.close();
+        }
+
+        return followers;
     }
 
-    return followers;
-  }
-
-  async getUsersFollowedByUser(userId: number): Promise<User[]> {
-    const session = driver.session();
-    let following: User[] = [];
-    try {
-      const result = await session.run(
-        `
+    async getUsersFollowedByUser(userId: number): Promise<User[]> {
+        const session = driver.session();
+        let following: User[] = [];
+        try {
+            const result = await session.run(
+                `
           MATCH (u:User)-[:FOLLOWS]->(following:User)
           WHERE u.id = $userId
           RETURN following
         `,
-        { userId }
-      );
+                { userId }
+            );
 
-      following = result.records.map((record) => {
-        const followingUser = record.get("following");
-        return {
-          id: followingUser.properties.id,
-          username: followingUser.properties.username,
-        };
-      });
-    } catch (error) {
-      logger.error("Error getting users followed by user:", error);
-      throw error;
-    } finally {
-      await session.close();
+            following = result.records.map((record) => {
+                const followingUser = record.get("following");
+                return {
+                    id: followingUser.properties.id,
+                };
+            });
+        } catch (error) {
+            logger.error("Error getting users followed by user:", error);
+            throw error;
+        } finally {
+            await session.close();
+        }
+
+        return following;
     }
-
-    return following;
-  }
 }
